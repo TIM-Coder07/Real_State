068d63b7af1a53d615f7ed2acd425507b6ea74f1
TIM-Coder07 <sajonwdd72566@gmail.com>
2025-02-13 16:23:27 +0600

    Add files via upload

---
+++ resposive.js
@@ -0,0 +1,17 @@
+// const menuToggle = document.querySelector(".menu-toggle");
+// const navLinks = document.querySelector("nav ul");
+// const menuItems = document.querySelectorAll("nav ul li a"); // Select all menu items
+
+// // Toggle menu on button click
+// menuToggle.addEventListener("click", () => {
+//     navLinks.classList.toggle("show");
+//     menuToggle.classList.toggle("active");
+// });
+
+// // Close menu when clicking a menu item
+// menuItems.forEach(item => {
+//     item.addEventListener("click", () => {
+//         navLinks.classList.remove("show");
+//         menuToggle.classList.remove("active");
+//     });
+// });
